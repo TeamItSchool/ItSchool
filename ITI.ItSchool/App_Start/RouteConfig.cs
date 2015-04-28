3696db5e9adfdb77ee633766f0771aaafc18fa83
Kikabou Guenole <kikabouguenole@gmail.com>
2015-04-28 11:30:33 +0200

    Start on Controller : Draw and launch : Painting

--- ITI.ItSchool/App_Start/RouteConfig.cs
+++ ITI.ItSchool/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ITI.ItSchool
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Draw", action = "Painting", id = UrlParameter.Optional }
             );
         }
     }
