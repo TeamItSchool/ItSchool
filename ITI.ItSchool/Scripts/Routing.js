367a744d0c0ab0b9529aa97df41d934fe6b35f40
Kikabou Guenole <kikabouguenole@gmail.com>
2015-05-22 19:24:02 +0200

    Major update here

--- ITI.ItSchool/Scripts/Routing.js
+++ ITI.ItSchool/Scripts/Routing.js
@@ -1,6 +1,6 @@
 ﻿angular.module('TheApp', ['ngRoute', 'ngMaterial']) // ['ngRoute'] is required for the routing and
     //['ngMaterial'] for the material design components
-.run(function ($log) {
+.run(function ($log, $rootScope) {
     $log.debug("startApp running");
 })
 .config(function ($routeProvider, $locationProvider) {
@@ -22,6 +22,14 @@
     .when('/teacher/login', {
         templateUrl: '/Templates/TeacherLoginPage.html',
         controller: 'TeacherLoginController'
+    })
+    .when('/teacher/registration', {
+        templateUrl: '/Templates/TeacherRegistrationPage.html',
+        controller: 'TeacherRegistrationController'
+    })
+    .when('/teacher/lobby', {
+        templateUrl: '/Templates/TeacherLobbyPage.html',
+        controller: 'TeacherLobbyController'
     })
     .when('/kid', {
         templateUrl: '/Templates/KidHomePage.html',
@@ -45,15 +53,205 @@
 .controller('HomeController', function ($scope) {
     $scope.Message = "Bienvenue";
 })
-.controller('KidLogInController', function ($scope) {
+.controller('KidLogInController', function ($scope, LoginService) {
     $scope.Message = "Embarque dans l'aventure It'School !";
+    $scope.IsLogedIn = false;
+    $scope.Submitted = false;
+    $scope.IsFormValid = false;
+
+    $scope.LoginData = {
+        Username: '',
+        Password: ''
+    };
+    //Check if Form is valid or not // here f1 is our form Name
+    $scope.$watch('LoginForm.$valid', function (newVal) {
+        $scope.IsFormValid = newVal;
+    });
+    $scope.Login = function () {
+        $scope.Submitted = true;
+        if ($scope.IsFormValid) {
+            LoginService.GetUser($scope.LoginData).then(function (d) {
+                if (d.data.UserName != null && d.data.Status == 3) {
+                    $scope.IsLogedIn = true;
+                    $scope.Message = "Bienvenue " + d.data.FullName;
+                }
+                else if (d.data.UserName != null && d.data.Status == 1) {
+                    alert("T'es pas censé pouvoir te connecter ici.. " + d.data.UserName + ", allez dégage, OUST, BASTA ! #Pèse")
+                }
+                else if (d.data.UserName != null && d.data.Status == 2) {
+                    alert("Oops vous êtes professeur mais êtes dans l'espace Kidz")
+                }
+                else {
+                    alert('Oops tu as entré le mauvais pseudo ou le mauvais mot de passe. Réessye pour te connecter.')
+                }
+            })
+        }
+    };
+})
+.controller("KidRegistrationController", function ($scope, RegistrationService) {
+    //Default Variable
+    $scope.submitText = "Save";
+    $scope.submitted = false;
+    $scope.Message = "Inscris-toi sur It'School :)";
+    $scope.message = "";
+    $scope.isFormValid = false;
+    $scope.User = {
+        UserName: '',
+        Password: '',
+        FullName: '',
+        EmailID: '',
+        Gender: '',
+        Status: '3',
+        UserID: ''
+    };
+
+    //Check form validation // here RegisterForm is our form name
+    $scope.$watch("RegisterForm.$valid", function (newValue) {
+        $scope.isFormValid = newValue;
+    })
+
+    //Save Data
+    $scope.SaveData = function (data) {
+        if ($scope.submitText == "Save") {
+            $scope.submitted = true;
+            $scope.message = "";
+
+            if ($scope.isFormValid) {
+                $scope.submitText = "Please wait...";
+                $scope.User = data;
+                RegistrationService.SaveFormData($scope.User).then(function (d) {
+                    alert(d);
+                    if (d == 'Success') {
+                        //Have to clear form here
+                        ClearForm();
+                    }
+                    $scope.submitText = "Save";
+                });
+            }
+            else {
+                $scope.message = 'Please fill required fields value';
+            }
+        }
+    }
+
+    //Clear form (reset)
+    function ClearForm() {
+        $scope.User = {};
+        $scope.RegisterForm.$setPristine(); // here registerForm is our form name
+        $scope.submitted = false;
+    }
 })
 .controller('TeacherHomeController', function ($scope) {
     $scope.Message = 'Page "Professeurs"';
 })
-.controller('TeacherLoginController', function ($scope) {
-    $scope.Message = "Entrez vos identifiants pour vous connecter.";
+
+.controller('TeacherLobbyController', function ($scope, LoginService) {
+    var monobjet_json = sessionStorage.getItem("objet");
+    var monobjet = JSON.parse(monobjet_json);
+    // Affichage dans la console
+    console.log(monobjet.data.FullName);
+    $scope.Message = "Bonjour " + monobjet.data.FullName;
+
 })
+.controller('TeacherLoginController', function ($scope, LoginService) {
+
+    sessionStorage.removeItem("objet");
+
+    $scope.Message = "Entrez vos identifiants pour vous connecter.";
+    $scope.IsLogedIn = false;
+    $scope.Submitted = false;
+    $scope.IsFormValid = false;
+
+    $scope.LoginData = {
+        Username: '',
+        Password: ''
+    };
+    //Check if Form is valid or not // here f1 is our form Name
+    $scope.$watch('LoginForm.$valid', function (newVal) {
+        $scope.IsFormValid = newVal;
+    });
+    $scope.Login = function () {
+        $scope.Submitted = true;
+        if ($scope.IsFormValid) {
+            LoginService.GetUser($scope.LoginData).then(function (d) {
+                if (d.data.UserName != null && d.data.Status == 2) {
+
+                    var monobjet_json = JSON.stringify(d);
+                    sessionStorage.setItem("objet", monobjet_json);
+
+                    var monobjet_json = sessionStorage.getItem("objet");
+                    var monobjet = JSON.parse(monobjet_json);
+                    // Affichage dans la console
+                    console.log(monobjet.data.FullName);
+                    
+                    $scope.IsLogedIn = true;
+                    $scope.Message = "Vous êtes bien connecté. Bienvenue " + d.data.FullName;
+                }
+                else if (d.data.UserName != null && d.data.Status == 1) {
+                    alert("T'es pas censé pouvoir te connecter ici.. " + d.data.UserName + ", allez dégage, OUST, BASTA ! #Pèse")
+                }
+                else if (d.data.UserName != null && d.data.Status == 3) {
+                    alert("Oops tu es dans l'espace de tes professeurs, connecte-toi dans l'espace Kidz")
+                }
+                else {
+                    alert("Votre pseudo ou votre mot de passe sont incorrects. Veuillez réessayer s'il vous plaît")
+                }
+            })
+        }
+    };
+})
+.controller('TeacherRegistrationController', function ($scope) {
+    //Default Variable
+    $scope.submitText = "Save";
+    $scope.submitted = false;
+    $scope.Message = "Inscription";
+    $scope.message = "";
+    $scope.isFormValid = false;
+    $scope.User = {
+        UserName: '',
+        Password: '',
+        FullName: '',
+        EmailID: '',
+        Gender: ''
+    };
+
+    //Check form validation // here RegisterForm is our form name
+    $scope.$watch("RegisterForm.$valid", function (newValue) {
+        $scope.isFormValid = newValue;
+    })
+
+    //Save Data
+    $scope.SaveData = function (data) {
+        if ($scope.submitText == "Save") {
+            $scope.submitted = true;
+            $scope.message = "";
+
+            if ($scope.isFormValid) {
+                $scope.submitText = "Please wait...";
+                $scope.User = data;
+                RegistrationService.SaveFormData($scope.User).then(function (d) {
+                    alert(d);
+                    if (d == 'Success') {
+                        //Have to clear form here
+                        ClearForm();
+                    }
+                    $scope.submitText = "Save";
+                });
+            }
+            else {
+                $scope.message = 'Please fill required fields value';
+            }
+        }
+    }
+
+    //Clear form (reset)
+    function ClearForm() {
+        $scope.User = {};
+        $scope.RegisterForm.$setPristine(); // here registerForm is our form name
+        $scope.submitted = false;
+    }
+})
+
 .controller('KidHomeController', function ($scope) {
     $scope.Message = 'Page "Élève"';
 })
@@ -62,4 +260,99 @@
 })
 .controller('ErrorController', function ($scope) {
     $scope.Message = "404 Not Found!";
-});
+})
+
+    // INSCRIPTION
+.controller('Registration', function ($scope, RegistrationService) {
+    //Default Variable
+    $scope.submitText = "Save";
+    $scope.submitted = false;
+    $scope.message = "";
+    $scope.isFormValid = false;
+    $scope.User = {
+        UserName: '',
+        Password: '', 
+        FullName: '',
+        EmailID: '',
+        Gender: ''
+    };
+
+    //Check form validation // here RegisterForm is our form name
+    $scope.$watch("RegisterForm.$valid", function (newValue) {
+        $scope.isFormValid = newValue;
+    })
+
+    //Save Data
+    $scope.SaveData = function (data) {
+        if ($scope.submitText == "Save") {
+            $scope.submitted = true;
+            $scope.message = "";
+
+            if ($scope.isFormValid) {
+                $scope.submitText = "Please wait...";
+                $scope.User = data;
+                RegistrationService.SaveFormData($scope.User).then(function (d) {
+                    alert(d);
+                    if (d == 'Success') {
+                        //Have to clear form here
+                        ClearForm();
+                    }
+                    $scope.submitText = "Save";
+                });
+            }
+            else {
+                $scope.message = 'Please fill required fields value';
+            }
+        }
+    }
+
+    //Clear form (reset)
+    function ClearForm() {
+        $scope.User = {};
+        $scope.RegisterForm.$setPristine(); // here registerForm is our form name
+        $scope.submitted = false;
+    }
+})
+
+//Factories
+
+    //Login Factory
+.factory('LoginService', function ($http) {
+    var fac = {};
+    var data = "";
+    fac.GetUser = function (d) {
+        return $http({
+            url: '/Data/UserLogin',
+            method: 'POST',
+            data: JSON.stringify(d),
+            headers: { 'content-type': 'application/json' }
+        })
+    };
+
+    return fac;
+})
+
+    //Registration Factory
+.factory('RegistrationService', function ($http, $q) {
+    //here q is an angularJS service which helps us to run asynchronous function and return result when processing is done
+    var fac = {};
+    fac.SaveFormData = function (data) {
+        var defer = $q.defer();
+        $http({
+            url: '/Data/Register',
+            method: 'POST',
+            data: JSON.stringify(data),
+            header: { 'content-type': 'application/json' }
+        }).success(function (d) {
+            //Success callback
+            defer.resolve(d);
+        }).error(function (e) {
+            //Failed callback
+            alert('Error!');
+            defer.reject(e);
+        });
+        return defer.promise;
+    }
+    return fac;
+})
+;
