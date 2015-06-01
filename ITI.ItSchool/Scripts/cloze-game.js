f9772603b244ca7a04bb92dd7e23505da8d345b2
Mika <michael.gungaram@gmail.com>
2015-06-01 14:49:32 +0200

    Looking for some improvements for the cloze exercise.

--- ITI.ItSchool/Scripts/cloze-game.js
+++ ITI.ItSchool/Scripts/cloze-game.js
@@ -1,12 +1,25 @@
-﻿var ClozeGame = (function () {
-    function ClozeGame() {
+﻿var word = (function () {
+    function word() {
     }
-    ClozeGame.prototype.check = function (word) {
-        if (word == "180") {
-            alert("Right!");
-        } else {
-            alert("Wrong...");
-        }
+    return word;
+})();
+
+var ClozeGame = (function () {
+    function ClozeGame() {
+        this.errors = 0;
+    }
+    ClozeGame.prototype.getWords = function () {
+    };
+
+    ClozeGame.prototype.check = function (word, word2, word3) {
+        if (word != "is")
+            this.errors++;
+        if (word2 != "are")
+            this.errors++;
+        if (word3 != "blue")
+            this.errors++;
+
+        return this.errors;
     };
     return ClozeGame;
 })();
@@ -15,8 +28,22 @@ var clozeGame = new ClozeGame();
 var button = document.createElement('button');
 button.textContent = "Vérifier réponse !";
 button.onclick = function () {
-    var word = document.getElementById('word').value;
-    clozeGame.check(word);
+    var word = document.getElementById('sentence1').value;
+    var word2 = document.getElementById('sentence2').value;
+    var word3 = document.getElementById('sentence3').value;
+
+    var errors = clozeGame.check(word, word2, word3);
+    switch (errors) {
+        case 0:
+            alert("Bravo ! Tu n'as fait aucune faute !");
+            break;
+        case 1:
+            alert("Très bien ! Tu as fait 1 faute.");
+            break;
+        default:
+            alert("Dommage... Tu as fait plus de 2 fautes. Réessaie encore !");
+            break;
+    }
 };
 document.body.appendChild(button);
 //# sourceMappingURL=cloze-game.js.map
--- ITI.ItSchool/Scripts/cloze-game.ts
+++ ITI.ItSchool/Scripts/cloze-game.ts
@@ -1,21 +1,47 @@
-﻿class ClozeGame
+﻿class word {
+    content: string;
+}
+
+class ClozeGame
 {
+    errors: number = 0;
+
     constructor() { }
 
-    check( word: string ) {
-        if (word == "180") {
-            alert("Right!");
-        } else {
-            alert( "Wrong..." );
-        }
+    getWords(): Array<word> {
+
+    }
+
+    check(word: string, word2: string, word3: string): number {
+
+        if (word != "is") this.errors++;
+        if (word2 != "are") this.errors++;
+        if (word3 != "blue") this.errors++;
+
+        return this.errors;
     }
 } 
 
 var clozeGame = new ClozeGame();
 var button = document.createElement('button');
 button.textContent = "Vérifier réponse !";
-button.onclick = function () {
-    var word = (<HTMLInputElement>document.getElementById('word')).value;
-    clozeGame.check( word );
+button.onclick = function() {
+    var word = (<HTMLInputElement>document.getElementById('sentence1')).value;
+    var word2 = (<HTMLInputElement>document.getElementById('sentence2')).value;
+    var word3 = (<HTMLInputElement>document.getElementById('sentence3')).value;
+
+
+    var errors = clozeGame.check(word, word2, word3);
+    switch(errors) {
+        case 0:
+            alert("Bravo ! Tu n'as fait aucune faute !");
+            break;
+        case 1:
+            alert("Très bien ! Tu as fait 1 faute.");
+            break;
+        default:
+            alert("Dommage... Tu as fait plus de 2 fautes. Réessaie encore !");
+            break;
+    }
 }
 document.body.appendChild(button);
