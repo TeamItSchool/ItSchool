e926b04a75bcf1b4666a2183a3ffcbeac93d30e1
Kikabou Guenole <kikabouguenole@gmail.com>
2015-06-12 02:49:55 +0200

    Adding GameContextMigration & SchoolContextMigration

--- ITI.ItSchool/GameContextMigrations/201506110401143_Initial.cs
+++ ITI.ItSchool/GameContextMigrations/201506110401143_Initial.cs
@@ -35,147 +35,147 @@ namespace ITI.ItSchool.GameContextMigrations
                 .Index(t => t.ChapterId)
                 .Index(t => t.LevelId)
                 .Index(t => t.ExerciseTypeId);
-            
+
             CreateTable(
                 "dbo.Chapters",
                 c => new
                     {
-                        ChapterId = c.Int(nullable: false),
-                        Name = c.String(nullable: false, maxLength: 45),
-                        ThemeId = c.Int(nullable: false),
-                        GradeId = c.Int(nullable: false),
-                        Remarks = c.String(maxLength: 200),
-                    })
-                .PrimaryKey(t => t.ChapterId)
-                .ForeignKey("dbo.Grades", t => t.ChapterId)
-                .ForeignKey("dbo.Themes", t => t.ThemeId, cascadeDelete: true)
-                .Index(t => t.ChapterId)
-                .Index(t => t.ThemeId);
+                        ChapterId = c.Int( nullable: false ),
+                        Name = c.String( nullable: false, maxLength: 45 ),
+                        ThemeId = c.Int( nullable: false ),
+                        GradeId = c.Int( nullable: false ),
+                        Remarks = c.String( maxLength: 200 ),
+                    } )
+                .PrimaryKey( t => t.ChapterId )
+                .ForeignKey( "dbo.Grades", t => t.ChapterId )
+                .ForeignKey( "dbo.Themes", t => t.ThemeId, cascadeDelete: true )
+                .Index( t => t.ChapterId )
+                .Index( t => t.ThemeId );
             
-            //CreateTable(
-            //    "dbo.Grades",
-            //    c => new
-            //        {
-            //            GradeId = c.Int(nullable: false, identity: true),
-            //            Name = c.String(nullable: false, maxLength: 45),
-            //            Remarks = c.String(maxLength: 200),
-            //        })
-            //    .PrimaryKey(t => t.GradeId);
+            ////CreateTable(
+            ////    "dbo.Grades",
+            ////    c => new
+            ////        {
+            ////            GradeId = c.Int(nullable: false, identity: true),
+            ////            Name = c.String(nullable: false, maxLength: 45),
+            ////            Remarks = c.String(maxLength: 200),
+            ////        })
+            ////    .PrimaryKey(t => t.GradeId);
             
-            //CreateTable(
-            //    "dbo.Users",
-            //    c => new
-            //        {
-            //            UserId = c.Int(nullable: false, identity: true),
-            //            FirstName = c.String(nullable: false, maxLength: 45),
-            //            LastName = c.String(nullable: false),
-            //            Nickname = c.String(nullable: false),
-            //            Mail = c.String(nullable: false),
-            //            Password = c.String(nullable: false),
-            //            GradeId = c.Int(nullable: false),
-            //            AvatarId = c.Int(nullable: false),
-            //            GroupId = c.Int(nullable: false),
-            //            Remarks = c.String(maxLength: 200),
-            //        })
-            //    .PrimaryKey(t => t.UserId)
-            //    .ForeignKey("dbo.Grades", t => t.GradeId, cascadeDelete: true)
-            //    .ForeignKey("dbo.Groups", t => t.GroupId, cascadeDelete: true)
-            //    .Index(t => t.GradeId)
-            //    .Index(t => t.GroupId);
+            ////CreateTable(
+            ////    "dbo.Users",
+            ////    c => new
+            ////        {
+            ////            UserId = c.Int(nullable: false, identity: true),
+            ////            FirstName = c.String(nullable: false, maxLength: 45),
+            ////            LastName = c.String(nullable: false),
+            ////            Nickname = c.String(nullable: false),
+            ////            Mail = c.String(nullable: false),
+            ////            Password = c.String(nullable: false),
+            ////            GradeId = c.Int(nullable: false),
+            ////            AvatarId = c.Int(nullable: false),
+            ////            GroupId = c.Int(nullable: false),
+            ////            Remarks = c.String(maxLength: 200),
+            ////        })
+            ////    .PrimaryKey(t => t.UserId)
+            ////    .ForeignKey("dbo.Grades", t => t.GradeId, cascadeDelete: true)
+            ////    .ForeignKey("dbo.Groups", t => t.GroupId, cascadeDelete: true)
+            ////    .Index(t => t.GradeId)
+            ////    .Index(t => t.GroupId);
             
-            //CreateTable(
-            //    "dbo.Avatars",
-            //    c => new
-            //        {
-            //            AvatarId = c.Int(nullable: false),
-            //            Name = c.String(nullable: false, maxLength: 45),
-            //            UserId = c.Int(nullable: false),
-            //            FootId = c.Int(nullable: false),
-            //            LegsId = c.Int(nullable: false),
-            //            BodyId = c.Int(nullable: false),
-            //        })
-            //    .PrimaryKey(t => t.AvatarId)
-            //    .ForeignKey("dbo.Bodies", t => t.BodyId, cascadeDelete: true)
-            //    .ForeignKey("dbo.Feet", t => t.FootId, cascadeDelete: true)
-            //    .ForeignKey("dbo.Legs", t => t.LegsId, cascadeDelete: true)
-            //    .ForeignKey("dbo.Users", t => t.AvatarId)
-            //    .Index(t => t.AvatarId)
-            //    .Index(t => t.FootId)
-            //    .Index(t => t.LegsId)
-            //    .Index(t => t.BodyId);
+            ////CreateTable(
+            ////    "dbo.Avatars",
+            ////    c => new
+            ////        {
+            ////            AvatarId = c.Int(nullable: false),
+            ////            Name = c.String(nullable: false, maxLength: 45),
+            ////            UserId = c.Int(nullable: false),
+            ////            FootId = c.Int(nullable: false),
+            ////            LegsId = c.Int(nullable: false),
+            ////            BodyId = c.Int(nullable: false),
+            ////        })
+            ////    .PrimaryKey(t => t.AvatarId)
+            ////    .ForeignKey("dbo.Bodies", t => t.BodyId, cascadeDelete: true)
+            ////    .ForeignKey("dbo.Feet", t => t.FootId, cascadeDelete: true)
+            ////    .ForeignKey("dbo.Legs", t => t.LegsId, cascadeDelete: true)
+            ////    .ForeignKey("dbo.Users", t => t.AvatarId)
+            ////    .Index(t => t.AvatarId)
+            ////    .Index(t => t.FootId)
+            ////    .Index(t => t.LegsId)
+            ////    .Index(t => t.BodyId);
             
-            //CreateTable(
-            //    "dbo.Bodies",
-            //    c => new
-            //        {
-            //            BodyId = c.Int(nullable: false, identity: true),
-            //            Name = c.String(nullable: false, maxLength: 45),
-            //            Link = c.String(maxLength: 512),
-            //        })
-            //    .PrimaryKey(t => t.BodyId);
+            ////CreateTable(
+            ////    "dbo.Bodies",
+            ////    c => new
+            ////        {
+            ////            BodyId = c.Int(nullable: false, identity: true),
+            ////            Name = c.String(nullable: false, maxLength: 45),
+            ////            Link = c.String(maxLength: 512),
+            ////        })
+            ////    .PrimaryKey(t => t.BodyId);
             
-            //CreateTable(
-            //    "dbo.Feet",
-            //    c => new
-            //        {
-            //            FootId = c.Int(nullable: false, identity: true),
-            //            Name = c.String(nullable: false, maxLength: 45),
-            //            Link = c.String(maxLength: 512),
-            //        })
-            //    .PrimaryKey(t => t.FootId);
+            ////CreateTable(
+            ////    "dbo.Feet",
+            ////    c => new
+            ////        {
+            ////            FootId = c.Int(nullable: false, identity: true),
+            ////            Name = c.String(nullable: false, maxLength: 45),
+            ////            Link = c.String(maxLength: 512),
+            ////        })
+            ////    .PrimaryKey(t => t.FootId);
             
-            //CreateTable(
-            //    "dbo.Legs",
-            //    c => new
-            //        {
-            //            LegsId = c.Int(nullable: false, identity: true),
-            //            Name = c.String(nullable: false, maxLength: 45),
-            //            Link = c.String(maxLength: 512),
-            //        })
-            //    .PrimaryKey(t => t.LegsId);
+            ////CreateTable(
+            ////    "dbo.Legs",
+            ////    c => new
+            ////        {
+            ////            LegsId = c.Int(nullable: false, identity: true),
+            ////            Name = c.String(nullable: false, maxLength: 45),
+            ////            Link = c.String(maxLength: 512),
+            ////        })
+            ////    .PrimaryKey(t => t.LegsId);
             
-            //CreateTable(
-            //    "dbo.Groups",
-            //    c => new
-            //        {
-            //            GroupId = c.Int(nullable: false, identity: true),
-            //            Name = c.String(nullable: false, maxLength: 45),
-            //            Remarks = c.String(maxLength: 200),
-            //        })
-            //    .PrimaryKey(t => t.GroupId);
-            
+            ////CreateTable(
+            ////    "dbo.Groups",
+            ////    c => new
+            ////        {
+            ////            GroupId = c.Int(nullable: false, identity: true),
+            ////            Name = c.String(nullable: false, maxLength: 45),
+            ////            Remarks = c.String(maxLength: 200),
+            ////        })
+            ////    .PrimaryKey(t => t.GroupId);
+
             CreateTable(
                 "dbo.Themes",
                 c => new
                     {
-                        ThemeId = c.Int(nullable: false, identity: true),
-                        Name = c.String(nullable: false, maxLength: 200),
-                        MatterId = c.Int(nullable: false),
-                        Remarks = c.String(maxLength: 200),
-                    })
-                .PrimaryKey(t => t.ThemeId)
-                .ForeignKey("dbo.Matters", t => t.MatterId, cascadeDelete: true)
-                .Index(t => t.MatterId);
+                        ThemeId = c.Int( nullable: false, identity: true ),
+                        Name = c.String( nullable: false, maxLength: 200 ),
+                        MatterId = c.Int( nullable: false ),
+                        Remarks = c.String( maxLength: 200 ),
+                    } )
+                .PrimaryKey( t => t.ThemeId )
+                .ForeignKey( "dbo.Matters", t => t.MatterId, cascadeDelete: true )
+                .Index( t => t.MatterId );
             
-            CreateTable(
-                "dbo.Matters",
-                c => new
-                    {
-                        MatterId = c.Int(nullable: false, identity: true),
-                        Name = c.String(nullable: false, maxLength: 45),
-                        Remarks = c.String(maxLength: 200),
-                    })
-                .PrimaryKey(t => t.MatterId);
-            
+            //CreateTable(
+            //    "dbo.Matters",
+            //    c => new
+            //        {
+            //            MatterId = c.Int(nullable: false, identity: true),
+            //            Name = c.String(nullable: false, maxLength: 45),
+            //            Remarks = c.String(maxLength: 200),
+            //        })
+            //    .PrimaryKey(t => t.MatterId);
+
             CreateTable(
                 "dbo.Levels",
                 c => new
                     {
-                        LevelId = c.Int(nullable: false, identity: true),
-                        Name = c.String(nullable: false, maxLength: 45),
-                        Remarks = c.String(maxLength: 200),
-                    })
-                .PrimaryKey(t => t.LevelId);
+                        LevelId = c.Int( nullable: false, identity: true ),
+                        Name = c.String( nullable: false, maxLength: 45 ),
+                        Remarks = c.String( maxLength: 200 ),
+                    } )
+                .PrimaryKey( t => t.LevelId );
             
         }
         
