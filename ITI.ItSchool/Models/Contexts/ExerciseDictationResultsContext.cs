cff21ac69d86cc67b1302aafe33fdd16ceef2403
Kikabou Guenole <kikabouguenole@gmail.com>
2015-07-01 18:48:42 +0200

    Adding ExercisesResults entity, ExerciseDictationResults entity plus their migrations

---
+++ ITI.ItSchool/Models/Contexts/ExerciseDictationResultsContext.cs
@@ -0,0 +1,21 @@
+﻿using ITI.ItSchool.Models.PlugExercicesResults;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.ItSchool.Models.Contexts
+{
+    public class ExerciseDictationResultsContext : DbContext
+    {
+        public ExerciseDictationResultsContext()
+            : base("ItSchool") 
+        {
+            Database.SetInitializer( new DropCreateDatabaseIfModelChanges<ExerciseDictationResultsContext>() );
+        }
+
+        public DbSet<ExerciseDictationResults> ExerciseDictationResults { get; set; }
+    }
+}
---
+++ ITI.ItSchool/Models/PlugExercicesResults/ExerciseDictationResults.cs
@@ -0,0 +1,27 @@
+﻿using ITI.ItSchool.Models.ExerciseEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.ItSchool.Models.PlugExercicesResults
+{
+    [Table("ExerciseDictationResults")]
+    public class ExerciseDictationResults
+    {
+        public int ExerciseDictationResultsId { get; set; }
+
+        [ForeignKey( "ExerciseDictationResultsId" )]
+        public virtual ExercisesResults ExercisesResults { get; set; }
+
+        public string SubmittedText { get; set; }
+
+        public string Remarks { get; set; }
+
+        public string Mark { get; set; }
+
+        public int Points { get; set; }
+    }
+}
